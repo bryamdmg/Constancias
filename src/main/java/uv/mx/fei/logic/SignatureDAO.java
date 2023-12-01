6319514d6210d518d19c5a0dcf90ab21d5b116c5
Luigi573 <xavier.arian@gmail.com>
2023-11-30 19:37:47 -0600

    UserManagement completed
    INSERT and UPDATE operations are possible

--- src/main/java/uv/mx/fei/logic/SignatureDAO.java
+++ src/main/java/uv/mx/fei/logic/SignatureDAO.java
@@ -15,38 +15,26 @@ public class SignatureDAO {
     public int updateSignature(String signature) throws SQLException{
         int result = 0;
         String query = "UPDATE Firmas SET firma = SHA2(?, 256)";
-        
-        try{
-            PreparedStatement statement = dbm.getConnection().prepareStatement(query);
-            
-            statement.setString(1, query);
-            
-            result = statement.executeUpdate();
-        }catch(SQLException exception){
-            throw new SQLException("Couldn't update data. There's no connection to DB");
-        }finally{
-            dbm.closeConnection();
-        }
-        
+        PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+        statement.setString(1, query);
+
+        result = statement.executeUpdate();
+        dbm.closeConnection();
+
         return result;
     }
     
     public String getSignature() throws SQLException{
         String signature = "";
         String query = "SELECT firma FROM Firmas";
-        
-        try{
-            PreparedStatement statement = dbm.getConnection().prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            
-            if(resultSet.next()){
-                signature = resultSet.getString("firma");
-            }
-        }catch(SQLException exception){
-            throw new SQLException();
-        }finally{
-            dbm.closeConnection();
+        PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+        ResultSet resultSet = statement.executeQuery();
+
+        if(resultSet.next()){
+            signature = resultSet.getString("firma");
         }
+        
+        dbm.closeConnection();
         
         return signature;
     }
--- src/main/java/uv/mx/fei/logic/UsersDAO.java
+++ src/main/java/uv/mx/fei/logic/UsersDAO.java
@@ -18,47 +18,37 @@ public class UsersDAO {
     public int addUser(User user) throws SQLException{
         int result = 0;
         String query = "INSERT INTO Usuarios(NumPersonal, nombre, tipoUsuario, fechaIngreso, fechaExpiración, gradoAcadémico, fechaNacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        
-        try{
-            PreparedStatement statement = dbm.getConnection().prepareStatement(query);
-            
-            statement.setInt(1, user.getStaffNumber());
-            statement.setString(2, user.getName());
-            statement.setString(3, user.getUserType());
-            statement.setDate(4, user.getJoinDate());
-            statement.setDate(5, user.getExpirationDate());
-            statement.setString(6, user.getAcademicDegree());
-            statement.setDate(7, user.getBirthDate());
-            
-            result = statement.executeUpdate();
-        }catch(SQLException exception){
-            throw new SQLException("Failed to add user. Couldn't connect to DB");
-        }
-        
+        PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+
+        statement.setInt(1, user.getStaffNumber());
+        statement.setString(2, user.getName());
+        statement.setString(3, user.getUserType());
+        statement.setDate(4, user.getJoinDate());
+        statement.setDate(5, user.getExpirationDate());
+        statement.setString(6, user.getAcademicDegree());
+        statement.setDate(7, user.getBirthDate());
+
+        result = statement.executeUpdate();
+        dbm.closeConnection();
+
         return result;
     }
     
     public int modifyUser(User user) throws SQLException{
         int result = 0;
         String query = "UPDATE TABLE Usuarios SET nombre = ?, tipoUsuario = ?, fechaIngreso = ?, fechaExpiración = ?, gradoAcadémico= ?, fechaNacimiento = ? WHERE NumPersonal IN(?)";
-        
-        try{
-            PreparedStatement statement = dbm.getConnection().prepareStatement(query);
-            
-            statement.setString(1, user.getName());
-            statement.setString(2, user.getUserType());
-            statement.setDate(3, user.getJoinDate());
-            statement.setDate(4, user.getExpirationDate());
-            statement.setString(5, user.getAcademicDegree());
-            statement.setDate(6, user.getBirthDate());
-            statement.setInt(7, user.getStaffNumber());
-            
-            result = statement.executeUpdate();
-        }catch(SQLException exception){
-            throw new SQLException("Failed to modify User. Couldn't connect to DB");
-        }finally{
-            dbm.closeConnection();
-        }
+        PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+
+        statement.setString(1, user.getName());
+        statement.setString(2, user.getUserType());
+        statement.setDate(3, user.getJoinDate());
+        statement.setDate(4, user.getExpirationDate());
+        statement.setString(5, user.getAcademicDegree());
+        statement.setDate(6, user.getBirthDate());
+        statement.setInt(7, user.getStaffNumber());
+
+        result = statement.executeUpdate();
+        dbm.closeConnection();
         
         return result;
     }
@@ -66,29 +56,23 @@ public class UsersDAO {
     public ArrayList<User> getUserList() throws SQLException {
         ArrayList<User> userList = new ArrayList();
         String query = "SELECT NumPersonal, nombre, tipoUsuario, fechaIngreso, fechaExpiración, gradoAcadémico, fechaNacimiento FROM Usuarios";
-        
-        try{
-            PreparedStatement statement = dbm.getConnection().prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            
-            while(resultSet.next()){
-                User user = new User();
-                
-                user.setStaffNumber(resultSet.getInt("NumPersonal"));
-                user.setAcademicDegree(resultSet.getString("nombre"));
-                user.setUserType(resultSet.getString("tipoUsuario"));
-                user.setJoinDate(resultSet.getDate("fechaIngreso"));
-                user.setExpirationDate(resultSet.getDate("fechaExpiración"));
-                user.setAcademicDegree(resultSet.getString("gradoAcadémico"));
-                user.setBirthDate(resultSet.getDate("fechaNacimiento"));
-                
-                userList.add(user);
-            }
-        }catch(SQLException exception){
-            throw new SQLException("Failed to modify User. Couldn't connect to DB");
-        }finally{
-            dbm.closeConnection();
+        PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+        ResultSet resultSet = statement.executeQuery();
+
+        while(resultSet.next()){
+            User user = new User();
+
+            user.setStaffNumber(resultSet.getInt("NumPersonal"));
+            user.setAcademicDegree(resultSet.getString("nombre"));
+            user.setUserType(resultSet.getString("tipoUsuario"));
+            user.setJoinDate(resultSet.getDate("fechaIngreso"));
+            user.setExpirationDate(resultSet.getDate("fechaExpiración"));
+            user.setAcademicDegree(resultSet.getString("gradoAcadémico"));
+            user.setBirthDate(resultSet.getDate("fechaNacimiento"));
+
+            userList.add(user);
         }
+        dbm.closeConnection();
         
         return userList;
     }
